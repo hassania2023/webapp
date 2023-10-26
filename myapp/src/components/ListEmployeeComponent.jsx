3286bcb08e0d83ae4dc5f4179d70485c5f8f9b46
elhassania <elhassaniaerrafiy2023@gmail.com>
2023-10-27 00:07:52 +0100

    Premier commit

---
+++ myapp/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+import EmployeeService from './EmployeeService';
+
+
+
+class  ListEmployeeComponent extends Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+               employees: []
+        }
+        this.addEmployee=this.addEmployee.bind(this);
+    }
+    componentDidMount(){
+        EmployeeService.getEmployees().then((res) =>{
+            this.setState({employees: res.data});
+        });
+    }
+    addEmployee() {
+        
+        this.props.history.push('/add-employee');
+      }
+      
+    render() {
+        return (
+            <div>
+                <h2 className="text-center"> Employees List</h2>
+                <div className="row">
+                <button
+  className="btn btn-primary"
+  onClick={() => window.open('/add-employee', '_blank')}
+  style={{ color: 'white' }}
+>
+  Add Employee
+</button>
+                </div>
+                <div className="row" >
+                    <table className= "table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th> Employee First Name</th>
+                                <th> Employee last Name</th>
+                                <th> Employee email Id</th>
+                                <th> Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                              this.state.employees.map(
+                                employee =>
+                                <tr key={employee.id}>
+                                    <td> {employee.firstName}</td>
+                                    <td> {employee.lastName}</td>
+                                    <td> {employee.emailId}</td>
+                                </tr>
+                              )  
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default  ListEmployeeComponent;
